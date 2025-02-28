2d7577724c1615ada3c38689f835434a7573e108
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-02-28 22:47:44 +0530

    init migrate db

--- src/db/schema_zod.ts
+++ src/db/schema_zod.ts
@@ -13,17 +13,18 @@ import { createSelectSchema } from 'drizzle-zod';
 
 export const UserSchemaZod = createSelectSchema(user, {
   createdAt: z.coerce.date(),
-  updatedAt: z.coerce.date()
+  updatedAt: z.coerce.date(),
+  banExpires: z.coerce.date().nullable()
 });
 export const AccountSchemaZod = createSelectSchema(account, {
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
-  accessTokenExpiresAt: z.coerce.date(),
-  refreshTokenExpiresAt: z.coerce.date()
+  accessTokenExpiresAt: z.coerce.date().nullable(),
+  refreshTokenExpiresAt: z.coerce.date().nullable()
 });
 export const VerificationSchemaZod = createSelectSchema(verification, {
-  createdAt: z.coerce.date(),
-  updatedAt: z.coerce.date(),
+  createdAt: z.coerce.date().nullable(),
+  updatedAt: z.coerce.date().nullable(),
   expiresAt: z.coerce.date()
 });
 export const ProjectSchemaZod = createSelectSchema(project);
