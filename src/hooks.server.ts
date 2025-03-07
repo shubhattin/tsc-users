27d51ecab3d06d1ed970a496cafa38e44bf2259f
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-03-07 09:47:57 +0530

    captcha header fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -13,7 +13,8 @@ export const handle: Handle = async ({ event, resolve }) => {
     return new Response(null, {
       headers: {
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+        'Access-Control-Allow-Headers':
+          'Content-Type, Authorization, X-Requested-With, X-Captcha-Response',
         'Access-Control-Allow-Credentials': 'true',
         'Access-Control-Allow-Origin': origin,
         Vary: 'Origin'
