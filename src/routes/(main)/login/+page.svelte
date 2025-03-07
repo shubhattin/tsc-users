359c1df10955758cf43629770e66b506e075915e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-03-07 09:30:36 +0530

    add turnstile

--- src/routes/(main)/login/+page.svelte
+++ src/routes/(main)/login/+page.svelte
@@ -12,7 +12,7 @@
   <title>Login | The Sanskrit Channel</title>
 </svelte:head>
 
-<div class="mt-14 flex flex-col items-center justify-center">
+<div class="mt-20 flex flex-col items-center justify-center">
   <button
     disabled={!callback}
     onclick={async () => {
@@ -29,7 +29,7 @@
     class="btn preset-outlined-secondary-700-300 flex gap-2 rounded-lg font-semibold"
     ><Icon src={GoogleIcon} class="inline-block text-[1.25rem]" />Login with Google</button
   >
-  <div class="mt-4">
+  <div class="mt-6 sm:mt-8">
     <Turnstile
       siteKey={PUBLIC_TURNSTILE_SITE_KEY}
       on:callback={async (event) => {
